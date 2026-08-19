356de4bbf3c16545913747092c2efb615cfa8349
guardvalker <guardvalker@users.noreply.github.com>
2026-08-19 01:57:11 -0300

    v1.5.1: fix safe-area en iOS (barra superior tapada por el notch)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "bonapp-gastos-v6";
+const CACHE = "bonapp-gastos-v7";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./config.js", "./sync.js"];
 
 self.addEventListener("install", (event) => {
