afbc85d45a62b8def5fb575630e531f9f26522fa
guardvalker <guardvalker@users.noreply.github.com>
2026-08-19 02:37:08 -0300

    v1.7.1: rehacer ícono de Gatitas, ahora se entiende como gato

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "bonapp-gastos-v9";
+const CACHE = "bonapp-gastos-v10";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./config.js", "./logic.js", "./charts.js", "./sync.js"];
 
 self.addEventListener("install", (event) => {
